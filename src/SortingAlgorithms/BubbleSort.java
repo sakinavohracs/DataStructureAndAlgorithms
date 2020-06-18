93894e0cb95b717978fb215831151298fb9f9edc
SakinaVohra <sakinavohracs@gmail.com>
2020-06-18 15:52:20 +0100

    Bubble Sort

---
+++ src/SortingAlgorithms/BubbleSort.java
@@ -0,0 +1,30 @@
+package SortingAlgorithms;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {22,34,-43,50,23,67,-112};
+
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex --){
+            for(int i = 0; i < lastUnsortedIndex; i++){
+                if(intArray[i] > intArray[i+1])
+                swap(intArray,i,i+1);
+            }
+        }
+        for(int i = 0; i < intArray.length;i++) {
+            System.out.println(i + " " + intArray[i]);
+        }
+    }
+
+    public static void swap(int[] array, int i, int j){
+
+        if(i == j){
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
